b8e31ce7fafdfe59f1e30e38a6f74c9ec3564bd5
Sara Chang <sarachang530@gmail.com>
2020-05-23 14:58:57 -0400

    unicafe feedback

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,38 +3,75 @@ import ReactDOM from 'react-dom';
 // import useState from 'react';
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1)
+  const handleGood = () => {
+    setGood(good + 1);
   };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
   };
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <p>{allClicks.join(' ')}</p>
-      </div>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGood} text='good'/>
+      <Button onClick={handleNeutral} text='neutral'/>
+      <Button onClick={handleBad} text='bad'/>
+      <p>Statistics</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  );
+  )
 };
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'));
+//     setLeft(left + 1)
+//   };
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'));
+//     setRight(right + 1);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         {left}
+//         <Button onClick={handleLeftClick} text='left' />
+//         <Button onClick={handleRightClick} text='right' />
+//         {right}
+//         <History allClicks={allClicks} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// const History = (props) => {
+//   if(props.allClicks.length === 0){
+//     return (
+//       <div>the app is used by pressing the buttons</div>
+//     )
+//   }
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+// const Button = ({ onClick, text }) => (
+//   <button onClick={onClick}>{text}</button>
+// )
+
 ReactDOM.render(<App />, document.getElementById('root'));
